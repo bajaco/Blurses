1a15e719732d1116b3ffb08637b6b647572686de
GregCM <gregcaceres@gmail.com>
2020-11-21 18:57:09 -0600

    quit key

--- blurses/__init__.py
+++ blurses/__init__.py
@@ -128,25 +128,18 @@ class Menu:
     def add(self, name):
         self.options.insert(-1, name)
 
-
     def input(self, key):
-        #with open('keycodes.txt', 'a') as f:
-            #f.write('\n%s: %s' % (chr(key), key))
-        # =================================
-        # Navigation
         if (key == curses.KEY_UP) or (key == ord('k')):
             if self.active - 1 >= 1:
                 self.active -= 1
         elif (key == curses.KEY_DOWN) or (key == ord('j')):
             if self.active < len(self.options) - 1:
                 self.active += 1
-        # =================================
-        # Search
-        elif key == ord('/'):
-            return self.active
-        # =================================
         elif key == curses.KEY_ENTER or key == 10: 
             return self.active
+        elif key == ord('q'):
+            # Quit
+            return 3
         return 0
             
 class Blurses:
